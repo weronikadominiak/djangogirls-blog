5743cdc4f7d38cb34bdba06affda6ec296af8cad
Weronika Felczak <felczak.weronika@gmail.com>
2018-08-12 13:33:59 +0200

    Extend your application

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
-urlpatterns =[
-    path('', views.post_list, name='post_list')
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
